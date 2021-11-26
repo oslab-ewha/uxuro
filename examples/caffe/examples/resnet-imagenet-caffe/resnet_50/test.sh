b8ebc66733fbd43111c754692f5cf4b708611583
KyungWoon Cho <cezanne@codemayo.com>
2021-11-26 12:13:27 +0900

    rearrange source tree

--- examples/caffe/examples/resnet-imagenet-caffe/resnet_50/test.sh
+++
@@ -1,1 +0,0 @@
-~/heyihui-local/caffe/build/tools/caffe test -iterations 1500 -model $1 -weights $2 -gpu $3
--- examples/caffe/examples/resnet-imagenet-caffe/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-./run_original_benchmark.sh
-./run_uvm_benchmark.sh
-./run_dragon_benchmark.sh
